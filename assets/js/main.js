4bc2c413510f75081b53f418d553f8afa1111c5a
shamimxpress <124999174+shamimxpress@users.noreply.github.com>
2023-02-11 01:43:17 +0600

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,34 +4,53 @@ var $user={};
 const data_map={
 	'ID': 'id',
 	'Username': 'username',
-	'Package': 'Package',
-	'Name': 'Name',
+	'Package': 'package',
+	'Name': 'name',
 	'Contact_Address': 'address',
 	'Contact': 'contact',
 	'Bill': 'bill'
 };
-fetch(url).then(res=>res.blob()).then((blob)=>{
-	// console.log(blob);
-	readXlsxFile(blob,{data_map}).then(function(rows) {
-		console.log(rows);
-		// rows.shift();
-		// var $user_list=$('#user_list');
-		// $user_list.html('');
-		// rows.forEach(function(row){
-		// 	// console.log(row);
-		// 	$user[row[3]]=row;
 
-		// 	var opt=`<option value="${row[3]}">`;
-		// 	// var opt=`<option value="${row[0]}">${row[3]}</option>`;
-		// 	// console.log(opt);
-		// 	$user_list.append(opt);
-		// });
-	})
+fetch(url).then(res=>res.blob())
+.then(blob=>readXlsxFile(blob,{map:data_map}))
+.then(({rows})=>{
+		// console.log(rows);
+		var opt_html='';
+		rows.forEach(function(row){
+			// console.log(row);
+			$user[row['id']]=row;
+			var opt=`<option value="${row['id']}">${row['Name']}</option>`;
+			// console.log(opt);
+			opt_html+=opt;
+			
+		});
+		
+		var $user_list=$('#user_list');
+		$user_list.html(opt_html);
+		// $user_list.append(opt_html);
 });
 var $user=$('#user');
 var $search=$('#search-btn');
+var $print=$('.print-body');
 $search.on('click',function(e){
 	// console.log($user.val());
-	console.log($user[$user.val()]);
-	
+	var user=$user[$user.val()];
+	console.log(user);
+	var receipt=`
+		<div class="mb-3 row">
+			<div class="col-md-4 col-form-label">Email</div>
+			<div class="col-md-6"></div>
+		</div>
+	name: ${user.name}<br>
+	package: ${user.package}<br>
+	address: ${user.address}<br>
+	bill: ${user.bill}<br>
+	contact: ${user.contact}<br>
+	id: ${user.id}<br>
+	username: ${user.username}<br>
+	`;
+	$print.html(receipt);
+});
+$('#print-btn').on('click',function(e){
+	window.print();
 });
